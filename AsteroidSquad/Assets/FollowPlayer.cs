25eb3505331d070b4ca988a1dbfe5fea0b53d0fb
matheuspb95 <matheuspb95@gmail.com>
2014-12-05 09:12:36 -0400

    Dia 05/12

--- AsteroidSquad/Assets/FollowPlayer.cs
+++ AsteroidSquad/Assets/FollowPlayer.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
-	public float distance;
+	public float range;
 	private GameObject player;
+	public int damage;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -11,12 +12,16 @@ public class FollowPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<ShootByTime>().canShoot = false;
-		if(Vector3.Distance(transform.position, player.transform.position) < distance){
-			GetComponent<ShootByTime>().canShoot = true;
+		if(Vector3.Distance(transform.position, player.transform.position) < range){
 			Vector3 target = player.transform.position - transform.position;
 			float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		if (c.gameObject.tag == "Player") {
+			c.gameObject.GetComponent<CheckCollisionWithAsteroids>().TakeDamage	(damage);
 		}
 	}
 }
--- AsteroidSquad/Assets/FollowPlayerAccel.cs
+++ AsteroidSquad/Assets/FollowPlayerAccel.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class FollowPlayerAccel : MonoBehaviour {
 	public float distance;
 	private GameObject player;
-	public float accel;
+	public float acceleracao;
+	public float maxVelocity;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -13,12 +14,16 @@ public class FollowPlayerAccel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector2 vel = rigidbody2D.velocity;
 		Vector3 target = player.transform.position - transform.position;
 		float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
-		float dirx = accel*Mathf.Cos(angle*Mathf.Deg2Rad)/100;
-		float diry = accel*Mathf.Sin(angle*Mathf.Deg2Rad)/100;
+		float dirx = acceleracao*Mathf.Cos(angle*Mathf.Deg2Rad)/100;
+		float diry = acceleracao*Mathf.Sin(angle*Mathf.Deg2Rad)/100;
 		Vector2 direction = new Vector2(dirx,diry);
-		rigidbody2D.AddForce (direction);
+		rigidbody2D.velocity += direction;
+		if (rigidbody2D.velocity.magnitude > maxVelocity) {
+			rigidbody2D.velocity = vel;
+		}
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		//rigidbody2D.rotation -= 90;
 	}
